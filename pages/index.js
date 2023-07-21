c9fe5cf0e5767b874cc8a0044abb2fd90674afca
Colton Trujillo <coltonbeau.io@gmail.com>
2023-07-21 09:38:44 -0600

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,62 @@
+import Head from 'next/head'
+import utilStyles from '../styles/utils.module.css'
+import { getCardNames, dealCards } from '../lib/cards';
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import Axios from 'axios';
+
+export async function getStaticProps() {
+  const allCards = getCardNames();
+  const table = dealCards();
+  return {
+    props: {
+      allCards,
+      table
+    },
+  };
+}
+
+export default function Home({ allCards, table }) {
+
+  function renderCards(cards){
+    return cards.map((card, i) => {
+      const imgSrc = `/card_images/${card}.PNG`;
+      return <Image src={imgSrc} width='150' height='200' alt={imgSrc} key={imgSrc}/>})
+  }
+
+  const callOutcomeAPI = () => {
+    Axios.get(getAPIURL(table.handOne,table.handTwo,table.cc)).then(res => {
+      console.log(res.data);
+      res.data.winners.map(winner =>(console.log(winner.result)));
+    });
+  }
+
+
+
+  return (
+    <div className={utilStyles.wrapper}>
+      <div className={utilStyles.topRow}>
+        <div className={utilStyles.topLeft}>
+      {renderCards(table.handOne)}
+       </div>
+       <div className={utilStyles.topRight}>
+       </div>
+      </div>
+      <div className={utilStyles.middleRow}>
+      {renderCards(table.cc)}
+      </div>
+      <div className={utilStyles.bottomRight}>
+      {renderCards(table.handTwo)}
+      </div>
+    </div>
+  )
+}
+
+
+function getAPIURL(playerOneHand, playerTwoHand, flop){
+  let pc1 = playerOneHand.join(',');
+  let pc2 = playerTwoHand.join(',');
+  let cc = flop.join(',');
+  const baseURL = `https://api.pokerapi.dev/v1/winner/texas_holdem?cc=${cc}&pc[]=${pc1}&pc[]=${pc2}`
+  return baseURL;
+}
